8f15e98760731f5a77e4de5f5fbc17cea1b005f9
Heimdim <dmitriyromanovets@mail.ru>
2018-04-10 00:01:51 +0300

    init commit

---
+++ Assets/init_anim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class init_anim : MonoBehaviour {
+
+    public Animator electronAnim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        electronAnim.GetComponent<Animator>();
+        electronAnim.Play("electron");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
